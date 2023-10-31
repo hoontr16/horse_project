c7d6410069033d35cbbe4ae910e9d014d17d71e6
Arvin Torabazari <atorabaz@umd.edu>
2023-10-31 18:50:09 -0400

    added favorite team function

--- hello_world.py
+++ hello_world.py
@@ -2,3 +2,8 @@ def hello_world(adj):
     print(f'Hello {adj} world!!')
 
 hello_world('cool')
+
+def favorite_team(team):
+    print (f"Arvins favorite team is the {team} ")
+
+favorite_team('baltimore ravens')
