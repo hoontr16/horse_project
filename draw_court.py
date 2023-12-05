8705ea5ece3d2c42c50c5594d2f3f0b33d5a6086
Arvin Torabazari <atorabaz@umd.edu>
2023-12-04 21:31:50 -0500

    added grid to court

--- draw_court.py
+++ draw_court.py
@@ -11,59 +11,64 @@ def draw_court(current_location):
             to the terminal. 
     """
     court = """
+             1   2   3   4   5   6   7   8   9  10  11  12  13  14  15  16  17  18  19  20  21  22
             ::::::^::::::::::::::::::::::::::^::::::::::::::::::^::::::::::::::::::::::::::^::::::
-            ~.    ~                         :^                  ::                         ~:   .^   
+         1  ~.    ~                         :^                  ::                         ~:   .^   
             ~.    ~                         .^    .:::^^^::.    :.                         ~.   .^
             ~.    ~                         .^       ^::~       :.                         ~.   .^    
-            ~.    ~                         .^        ..        :.                         ~.   .^  
+         2  ~.    ~                         .^        ..        :.                         ~.   .^  
             ~.    ~                         .^                  :.                         ~.   .^    
             ~.    ~                         .^                  :.                         ~.   .^  
-            ~.    ~                         .^                  :.                         ~.   .^    
+         3  ~.    ~                         .^                  :.                         ~.   .^    
             ~.    ~                         :^                  ::                         ~.   .^    
             ~.    ~                         :^                  ::                         ~.   .^    
-            ~.    ~                         :^       .  .       ::                         ~.   .^    
+         4  ~.    ~                         :^       .  .       ::                         ~.   .^    
             ~.    ~                         :^  .:. :.  .:. :.  ::                         ~.   .^    
             ~.    ^:                        .^  :.          .:  ::                        .~    .^    
-            ~.     ^:                       .~^                ^^.                       .~     .^   
+         5  ~.     ^:                       .~^                ^^.                       .~     .^   
             ~.      ^:                      :~                  ^:                      :^      .^    
             ~.       :^.                    :!::::::::::::::::::~:                     ^:       .^    
-            ~.        .^:                   .~                  ~.                   :^.        .^    
+         6  ~.        .^:                   .~                  ~.                   :^.        .^    
             ~.          .^:                  ^^                :^                  :^:          .^   
             ~.            .^:.                :^.            .^:                .:^:            .^    
-            ~.              .:^:.               :^:...  ...:^:.              .:::.              .^    
+         7  ~.              .:^:.               :^:...  ...:^:.              .:::.              .^    
             ~.                 .:::.              ...::::...              .:::.                 .^    
             ~.                    ..:::..                            ..::::.                    .^    
-            ~.                         ..::::......        ......::::...                        .^    
+         8  ~.                         ..::::......        ......::::...                        .^    
             ~.                               .....::::::::::.....                               .^    
             ~.                                                                                  .^    
+         9  ~.                                                                                  .^    
             ~.                                                                                  .^    
             ~.                                                                                  .^    
+         10 ~.                                                                                  .^        
             ~.                                                                                  .^    
-            ~.                                                                                  .^    
-            ~.                                                                                  .^    
-            ~.                                                                                  .^    
-            ~.                                                                                  .^    
+            ~.                                                                                  .^
+         11 ~.                                                                                  .^      
             ~.                                     .......                                      .^  
             ~.                                 .:::.......::.                                   .^    
-            ~.                                ::.           .::                                 .^    
+         12 ~.                                ::.           .::                                 .^    
             ~.                               ^.               .^                                .^    
             ~.                              ^.                 ::                               .^    
-            ^^::::::::::::::::::::::::::::::~:::::::::::::::::::^:::::::::::::::::::::::::::::::^:  """ #ASC II representation of court 
+         13 ^^::::::::::::::::::::::::::::::~:::::::::::::::::::^:::::::::::::::::::::::::::::::^:  """ #ASC II representation of court 
     new_court = " "
     marker = "X"
     counts = 0
+    x, y = current_location #unpacking the current location sequence into x and y variables.
+    line_number = 3 * (y) + 1
+    if not (0 < y < 14 and 0 < x < 23): #Ensures that the location specified is within the boundaries of the court and continues if it is.
+        print("Out of bounds, please try again.") 
+        return
     for line in court.split("\n"):
         counts += 1
-        if counts == current_location[0]: # If counts is equivalent to index 0 of the current location (x-axis) then continue
-            if 2 < current_location[0] < 39 and 13 < current_location[1] < 96: #Ensures that the location specified is within the boundaries of the court and continues if it is.
-                new_line = line[:current_location[1]] + marker + line[current_location[1] + 1:] #splices through ther line and adds the marker on the specification of index 1 (y-axis)
-                new_court += new_line + "\n" #adds the new line with the marker to the new court 
-            else:
-                print("Out of bounds, please try again.") 
-                return
+        if counts == line_number: # If counts is equivalent to index 0 of the current location (x-axis) then continue
+            index = 4 * x + 9 
+            new_line = line[:index] + marker + line[index + 1:] #splices through ther line and adds the marker on the specification of index 1 (y-axis)
+            new_court += new_line + "\n" #adds the new line with the marker to the new court 
         else:
             new_court += line + "\n" #add the unchanged lines to the new court 
+            
+        
     
     print(new_court)
 
-draw_court((3, 25))
+draw_court((6, 11))
