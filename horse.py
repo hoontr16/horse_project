c0c1b257c227cf36ad9575c9c48df850a0c61af3
hdh1216 <hdh1216@penguin.lxd>
2023-12-11 17:52:51 -0500

    copied changes to horse from demo

--- horse.py
+++ horse.py
@@ -433,38 +433,32 @@ class Coordinate:
         self.prob2 = dist_prob(normalize_dist(self.h, court_len, court_width))
         self.prob = (self.prob1 * self.prob2)
         
-        #defines type bonuses for different shots
-        type_bonuses = {
-            "lay-up": 0.10,
-            "free-throw": 0.075,
-            "three-pointer": -0.05,
-            "two-pointer": 0.0
+        #defines conditions and type bonuses for different shots
+        shot_conditions = {
+        self.h <= 1: "lay-up",
+        self.py == 6 and self.px in range(9, 15): "free-throw",
+        self.h >= 9: "three-pointer"
         }
         
-        #determines shot type based on conditions and then applies the bonuses
-        if self.h <=1:
-            key = "lay-up"  
-            self.type = key
-            bonus = type_bonuses.get(key)
-            self.prob += bonus     
+        type_bonuses = {
+        "lay-up": 0.10,
+        "free-throw": 0.075,
+        "three-pointer": -0.05,
+        "two-pointer": 0.0
+        }
         
-        if self.py ==6 and self.px in range (9,15):
-            key = "free-throw"  
-            self.type = key
-            bonus = type_bonuses.get(key)
-            self.prob += bonus     
+        #determines shot type based on conditions 
+        for condition, shot_type in shot_conditions.items():
+            if condition:
+                key = shot_type
+                break
+            else:  
+                key = "two-pointer"
         
-        if self.h >=9:
-            key = "three-pointer"  
-            bonus = type_bonuses.get(key)
-            self.type = key
-            self.prob += bonus  
-        
-        else:
-            key = "two-pointer"  
-            self.type = key
-            bonus = type_bonuses.get(key)
-            self.prob += bonus     
+        #applies the bonuses
+        self.type = key
+        bonus = type_bonuses.get(key)
+        self.prob += bonus
         
         return self.prob
 
