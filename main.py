ad19e2153fab4de4501072c77ff8b47e3290eaca
Yash <ykhanna1@terpmail.umd.edu>
2023-12-10 23:43:37 -0500

    Updated main.py to call display scores so that the letters assign to each player

--- main.py
+++ main.py
@@ -2,22 +2,21 @@ from player import Player, ComputerPlayer
 from random import choice
 from draw_court import draw_court
 
-def 
 
 def main():
-    """ The actual game loop.
+    """ Create and run the game.
     """
-    players = []
-    humans = input("How many human players?")
-    comps = input("How many computer players?")
-    for i in range(humans):
-        players.append(Player(input(f"Player {len(players) + 1}, what is your name? "), (0, 0)))
-    for j in range(comps):
-        players.append(ComputerPlayer(f"Computer {j}", (0, 0)))
-    cp = choice(players)
-    while True:
-        c = cp.pick_location()
-        draw_court(c)
-        cp.shoot()
+    gs = GameState()
+    n = 0
+    while gs.check_win() is None:
+        print(f"It's {gs.p1.name}'s turn to pick")
+        gs.turn()
+        n += 1
+        print(f"It's turn {n}") if n % 10 == 0 else None
+        gs.display_scores()
+        a = input("Press Enter to continue  ")
+
+    print(f"{gs.check_win().name} wins!")
+        
         
     
