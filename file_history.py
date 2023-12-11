d0132521f1f009c0e2115bc9091ab314d80258b1
Darren Hollis <dhollis1@terpmail.umd.edu>
2023-12-11 18:20:10 -0500

    committed file_history

---
+++ file_history.py
@@ -0,0 +1,12 @@
+def history_to_file(self, filename):
+    """Writes the shot history for each player to a file.
+
+        Arguments:
+            filename (str): The name of the file to write the shot history."""
+            
+    with open(filename, 'w') as f:
+        for player in self.players:
+            f.write(f"{player.name}'s shot history: {player.shot_history}\n"))
+        
+    
+    
